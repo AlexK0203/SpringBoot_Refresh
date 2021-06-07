c886d9a1f660e7c4c17d6326f7c4918ba4864f77
alexanderkindle <alexanderkindle@alexanders-mbp.fritz.box>
2021-06-07 09:07:32 +0200

    First Commit of Frontend Application

---
+++ guestbook-frontend/src/main/java/de/applicity/guestbook/GuestbookFrontendApplication.java
@@ -0,0 +1,13 @@
+package de.applicity.guestbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuestbookFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuestbookFrontendApplication.class, args);
+	}
+
+}
